079b9e43046f8464ac2e9e2b9b5ec0f04ba082e3
FrostyByteFX <76922754+FrostyByteFX@users.noreply.github.com>
2021-05-22 23:59:30 +0300

    Update summonername.js

--- summonername.js
+++ summonername.js
@@ -4,7 +4,7 @@ var region = document.getElementById("region").value;
 Region tulee käyttäjän valitseman alueen perusteella ja .value ottaa option tagissä määritetyn value arvon, joka on siis eri kuin käyttäjän näkemä teksti dropdown valikossa.
 Regioneita on League of Legends tai LoL pelissä monia ja peliä pelaavat yleensä tietävät oman regionin nimensä lyhenteen avulla esimerkiksi EUW on Europe West. 
 */
-const APIKEY = "RGAPI-b7de62f4-8513-42ea-ad14-832b6c70454a";
+const APIKEY = "RGAPI-e2cccd27-ecdf-4228-9c7e-0cfbcd6497b9";
 /*
 APIKEY täytyy päivittää 24h välein niin kauan kun sivustosta ei ole ilmoitettu Riot Games yhtiölle. Tulevaisuudessa tekisin node.js avulla serverin sivustolle, joka hoitaisi 
 kaikki Riot Games API tietokannasta pyydetyt haut ja APIKEY olisi talletettuna johonkin .env tiedostoon, jossa olisi piilotettuna APIKEY enviroment variablena eli APIKEY ei olisi
